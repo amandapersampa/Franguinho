8f67a19514ea62a1d26d9ba6e7b60f981be03331
amandapersampa <amandapersampa@hotmail.com>
2017-02-23 10:58:58 -0300

    Criação do servidor Restful

--- src/main/java/br/edu/ifes/serra/config/MvcConfiguration.java
+++ src/main/java/br/edu/ifes/serra/config/MvcConfiguration.java
@@ -17,15 +17,6 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
 	@Bean
 	public ViewResolver getViewResolver(){
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
 		return resolver;
 	}
-	
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-	}
-
-	
 }
--- src/main/java/br/edu/ifes/serra/controller/HomeController.java
+++ src/main/java/br/edu/ifes/serra/controller/HomeController.java
@@ -1,18 +1,26 @@
 package br.edu.ifes.serra.controller;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
 
 import javax.servlet.http.HttpServletResponse;
 
+import br.edu.ifes.serra.model.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class HomeController {
 
-	@RequestMapping(value="/")
-	public ModelAndView test(HttpServletResponse response) throws IOException{
-		return new ModelAndView("home");
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+		return new Greeting(counter.incrementAndGet(),
+				String.format(template, name));
 	}
 }
