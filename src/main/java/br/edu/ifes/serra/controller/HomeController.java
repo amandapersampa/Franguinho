9075c4c3027737ac8038c5787b70cdc171d48a94
amandapersampa <amandapersampa@hotmail.com>
2017-03-14 08:28:40 -0300

    removendo hibernate

--- src/main/java/br/edu/ifes/serra/controller/HomeController.java
+++ src/main/java/br/edu/ifes/serra/controller/HomeController.java
@@ -1,16 +1,12 @@
 package br.edu.ifes.serra.controller;
 
-import java.io.IOException;
 import java.util.concurrent.atomic.AtomicLong;
 
-import javax.servlet.http.HttpServletResponse;
-
-import br.edu.ifes.serra.model.Greeting;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+
+import br.edu.ifes.serra.model.Greeting;
 
 @RestController
 public class HomeController {
@@ -18,7 +14,7 @@ public class HomeController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping("/greeting")
+	@RequestMapping("/")
 	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
 		return new Greeting(counter.incrementAndGet(),
 				String.format(template, name));
