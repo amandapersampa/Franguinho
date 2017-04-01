a0b13e24ee3ba1efb8fa73ce23d7dc4ebf611174
mbbotelho <mbbotelho23@gmail.com>
2017-04-01 12:37:08 -0300

    Criação da features de cadastro produto
    cadastro das features de estoque produto

---
+++ backend/src/test/java/br/edu/ifes/serra/controller/CadastroProdutoStep.java
@@ -0,0 +1,57 @@
+package br.edu.ifes.serra.controller;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CadastroProdutoStep {
+	@Given("^produto ainda nao foi cadastrado$")
+	public void produto_ainda_nao_foi_cadastrado() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Eu quero cadastrar o produto$")
+	public void eu_quero_cadastrar_o_produto() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^mensagem de sucesso$")
+	public void mensagem_de_sucesso() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^produto já foi cadastrado$")
+	public void produto_já_foi_cadastrado() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Eu quero cadastrar o produto existente$")
+	public void eu_quero_cadastrar_o_produto_existente() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^mensagem de produto já cadastrado$")
+	public void mensagem_de_produto_já_cadastrado() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Eu quero cadastrar o produto sem nome$")
+	public void eu_quero_cadastrar_o_produto_sem_nome() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^mensegem de erro no preenchimento do produto$")
+	public void mensegem_de_erro_no_preenchimento_do_produto() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
---
+++ backend/src/test/java/resources/feature/CadatroProduto.feature
@@ -0,0 +1,18 @@
+Feature: Cadastro de produto
+
+O cliente vai cadatrar o produto 
+    
+Scenario: produto foi cadastrado com sucesso
+    Given produto ainda nao foi cadastrado
+    When  Eu quero cadastrar o produto 
+    Then  mensagem de sucesso 
+  
+Scenario: produto já cadastrado
+    Given produto já foi cadastrado
+    When  Eu quero cadastrar o produto existente
+    Then  mensagem de produto já cadastrado
+
+Scenario: produto não foi cadastrado
+    Given produto ainda nao foi cadastrado
+    When  Eu quero cadastrar o produto sem nome
+    Then  mensegem de erro no preenchimento do produto
