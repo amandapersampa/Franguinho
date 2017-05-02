405198c68d1ca9fff0976620108bac884a9ad5a4
amandapersampa <amandapersampa@hotmail.com>
2017-05-02 20:16:49 -0300

    criação do teste de controller

---
+++ backend/src/test/java/br/edu/ifes/serra/controller/CardapioControllerTest.java
@@ -0,0 +1,15 @@
+package br.edu.ifes.serra.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes={CardapioControllerTest.class})
+public class CardapioControllerTest{
+
+	private MockMvc mockMvc;
+
+	
+}
