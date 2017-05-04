f6fbe93b753fc7cf7cd991e25ef1637ceee103ba
mbbotelho <mbbotelho23@gmail.com>
2017-05-03 21:02:21 -0300

    22-estoque
    Criação de itemEstoque

---
+++ backend/src/test/java/br/edu/ifes/serra/controller/CadastroItemCardapioStep.java
@@ -0,0 +1,65 @@
+package br.edu.ifes.serra.controller;
+
+import br.edu.ifes.serra.model.ItemDiverso;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CadastroItemCardapioStep {
+	
+	@Given("^ItemEstoque ainda nao foi cadastrado$")
+	public void itemestoque_ainda_nao_foi_cadastrado() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^Eu quero cadastrar ItemEstoque$")
+	public void eu_quero_cadastrar_ItemEstoque() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^Mensagem de sucesso$")
+	public void mensagem_de_sucesso() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^Cardapio Principal ainda nao foi cadastrado$")
+	public void cardapio_Principal_ainda_nao_foi_cadastrado() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^Eu quero cadastrar uma Cardapio Principal faltando os campos obrigatorios$")
+	public void eu_quero_cadastrar_uma_Cardapio_Principal_faltando_os_campos_obrigatorios() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^Mensagem de erro$")
+	public void mensagem_de_erro() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^Cardapio extra ainda nao foi cadastrado$")
+	public void cardapio_extra_ainda_nao_foi_cadastrado() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^Eu quero cadastrar uma Cardapio extra$")
+	public void eu_quero_cadastrar_uma_Cardapio_extra() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^Eu quero cadastrar uma Cardapio extra faltando campos obrigatorios$")
+	public void eu_quero_cadastrar_uma_Cardapio_extra_faltando_campos_obrigatorios() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+}
