6d491e9d3cd9ed6e168ce2e72afb17dcffd404cb
Unknown <mbbotelho23@gmail.com>
2017-07-02 17:49:49 -0300

    Inicio da criação da tela de pedido

--- app/main/forms/modal_item_cardapio.py
+++ app/main/forms/modal_item_cardapio.py
@@ -1,10 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import *
 from wtforms.validators import DataRequired
+from werkzeug.datastructures import MultiDict
 
-class Item_cardapio_forms(FlaskForm):
+class modal_item_cardapio(FlaskForm):
     item_cardapio = SelectField('item_cardapio')
     quantidade = IntegerField("quatidade", validators=[DataRequired()])
     lista_item_exta =[]
+
+    def reset(self):
+        blankData = MultiDict([('csrf_token', self.reset)])
+        self.process(blankData)
+
+
 
 
